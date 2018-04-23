059e8a299050c8aab0b41f9e40934dd473f2b48c
Michael Boroski <mjboroski@gmail.com>
2018-04-23 18:43:14 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,10 +1,18 @@
 class EmailParser
 
   attr_accessor :emails
-  #  that accepts a string of unformatted 
+
+  @@all={}
+  #  that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
+  def initialize(emails)
 
+  end
+
+  def self.parse
+
+  end
 end
