44446f4eba3688e349b435288219547021e20317
Michael Boroski <mjboroski@gmail.com>
2018-04-23 19:24:20 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,29 +2,13 @@ class EmailParser
 
   attr_accessor :emails
 
-  @@all=[]
-  @@emails=[]
-  #  that accepts a string of unformatted
-# emails. The parse method on the class should separate them into
-# unique email addresses.
-
   def initialize(emails)
-    @@all<<emails.to_s
+    @emails=emails
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.emails
-    @@emails
-  end
-
-  def self.parse
-    self.all.each.to_s do |entry|
-      self.emails<<entry.split(' ').split(',').to_s
-      binding.pry
+  def parse
+    emails.split.map do |email|
+      email.split(',')
     end
-    self.emails
   end
 end
